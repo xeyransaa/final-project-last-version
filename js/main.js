08ea48d98bcae48717db29aa9e6d6c3f156f1347
xeyransa <kheyransa.aliyeva@gmail.com>
2021-11-29 18:16:13 +0400

    Final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var btn = document.querySelectorAll(".main .contain .features button")
-var div = document.querySelectorAll(".main .contain .features .panel div")
+var div = document.querySelectorAll(".main .contain .features .panel .alt")
+var pwdTag = document.querySelector(".main .contain .acc-form .user-input .input #pwd")
+var seePwd = document.querySelector(".main .contain .acc-form .user-input .input i")
+var upBtn = document.querySelector(".up-arrow a")
 for (let i=0; i<btn.length;i++){
     btn[i].onclick=function(){
         for(let j=0; j<div.length;j++){
@@ -10,3 +13,30 @@ for (let i=0; i<btn.length;i++){
         this.classList.add("active")
     }
 }
+
+// seePwd.onclick=function(){
+//     if (this.className != "far fa-eye active"){
+//         pwdTag.type="text";
+//         this.classList.add("active");
+//     }
+//     else{
+//         pwdTag.type="password";
+//         this.classList.remove("active")
+//     }
+    
+// }
+
+window.onscroll=function(){
+    if (document.documentElement.scrollTop > 350){
+        upBtn.style.opacity="1"
+        upBtn.style.transform="translateY(0)"
+    }
+    else{
+        upBtn.style.opacity="0"
+        upBtn.style.transform="translateY(10px)"
+    }
+    console.log(document.documentElement.scrollTop);
+}
+
+    
+
